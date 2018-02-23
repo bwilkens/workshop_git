ffe44cbbc6ac2ca7fc780c7464a6debd2180c9df
Berend Wilkens <bwilkens@users.noreply.github.com>
2018-02-23 11:40:44 +0100

    Update Main.java

--- src/workshop_git/Main.java
+++ src/workshop_git/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		System.out.println("I don't feel like doing this workshop but I have to because it is 5 percent of my grade");
 		System.out.println("I would be happier if I could sleep the day away");
 		System.out.println("moh git, moh problems");
-		System.out.println("out of lame jokes");
+		System.out.println("In GitHub gewijzigd");
 	}
 
 }
