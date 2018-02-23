b34b60fcfa6fd5ba857ac8f28d97d75206e1130a
Berend Wilkens <berend.wilkens@hu.nl>
2018-02-23 10:55:06 +0100

    Master commit long text

--- src/workshop_git/Main.java
+++ src/workshop_git/Main.java
@@ -3,7 +3,7 @@ package workshop_git;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Cleanup branch");
+		System.out.println("I don't feel like doing this workshop but I have to because it is 5 percent of my grade");
 	}
 
 }
